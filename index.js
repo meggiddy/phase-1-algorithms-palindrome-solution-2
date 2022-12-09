a1bbab34f372a610c16ee87fa3f506d4e771fb33
meggidy <mimamitchelle@gmail.com>
2022-12-09 09:57:54 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from the beginning of the string to the middle of the string
+  for (let i = 0; i < word.length / 2; i++) {
+    const j = word.length - 1 - i;
+    if (word[i] !== word[j]) {
+      // if the letters don't match, return false
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
